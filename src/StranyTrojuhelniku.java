81048dbee58178e44b0c51a85da8341a2c09ff00
GurishMan <adam.gurica101@gmail.com>
2023-04-20 18:16:19 +0200

    .

--- src/StranyTrojuhelniku.java
+++ src/StranyTrojuhelniku.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class StranyTrojuhelniku {
+public class StranyTrojuhelniku {
+    private int A;
+    private int B;
+    private int C;
+
+    public StranyTrojuhelniku(int a, int b, int c) {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int a) {
+        A = a;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public void setB(int b) {
+        B = b;
+    }
+
+    public int getC() {
+        return C;
+    }
+
+    public void setC(int c) {
+        C = c;
+    }
 }
+
